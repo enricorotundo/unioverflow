66d66261a2d41b11b1606ed4964a3b060749d071
Federico Poli <fedux11@gmail.com>
2014-05-03 14:43:22 +0200

    Riscritto index utilizzando il metodo ->param(...)

--- Controller/Index.pm
+++ Controller/Index.pm
@@ -2,8 +2,6 @@ package Controller::Index;
 use strict;
 use warnings;
 use CGI::Carp;
-use Encode;
-use POSIX;
 
 use Lib::Renderer;
 use Middleware::Authentication;
@@ -12,11 +10,10 @@ use Middleware::Authentication;
 sub handler {
 	# Get parameters
 	my ($req, $res) = @_;
-	my %param = &query_string_variables;
 	
 	# TODO ...
 
-	my $page = $param{"page"} || 1;
+	my $page = $req->param{"page"} || 1;
 	my $questionsPerPage = 3;
 	# my $totalQuestions = Model::Question::countQuestions();
 	my $totalQuestions = 5;
@@ -43,41 +40,6 @@ sub handler {
 
 	# Response
 	$res->write(Lib::Renderer::render('index.html', $data));
-}
-
-
-# Fetches and parses query-string variables.
-# Returns an associative array like $param{$key} = $value.
-sub query_string_variables {
-
-	my $query_string = '';
-	if ($ENV{'REQUEST_METHOD'}) {
-		$query_string = $ENV{'QUERY_STRING'};
-	}
-
-	my %param;
-	my @pairs = split(/[&;]/, $query_string);
-
-	foreach (@pairs) {
-		my($key, $value) = split(/=/, $_, 2);
-
-		next if !defined $key;
-
-		$key =~ tr/+/ /;
-		$key =~ s/%([0-9a-fA-F]{2})/chr(hex($1))/ge;
-		$key = Encode::decode_utf8($key);
-
-		next if ($key eq '');
-
-		if (defined $value) {
-			$value =~ tr/+/ /;
-			$value =~ s/%([0-9a-fA-F]{2})/chr(hex($1))/ge;
-			$value = Encode::decode_utf8($value);
-		}
-
-		$param{$key} = $value;
-	}
-	return %param;
 }
 
 1;
