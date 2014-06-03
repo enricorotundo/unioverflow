50a5b687d32b2a607ad294d486dbaf63363b3645
SerenaG <girardiserena@gmail.com>
2014-06-03 16:48:12 +0200

    Javascript per controllo dati in form registrazione

---
+++ public_html/js/utils.js
@@ -0,0 +1,27 @@
+
+function validateForm() {
+	var campoErrori = document.getElementById("errori");
+    var email = document.forms["registrati-form"]["email"].value;
+    var password = document.forms["registrati-form"]["password"].value;
+    var passwordconfirm = document.forms["registrati-form"]["passwordconfirm"].value;
+    var erroriForm = "";
+
+    // ^[a-z0-9._%+-]{1,64}\@studenti.unipd.it$ : controlla che sia una mail valida
+    // m : multiline
+    if (email==null || !(new RegExp("^[a-z0-9._%+-]{1,64}\@studenti.unipd.it$", "m").test(email))  ) {
+    	erroriForm = "<li>L'email deve finire con '\@studenti.unipd.it'</li>";
+    }
+    if(password==null || password=="" || password.length < 8){
+    	erroriForm = erroriForm + "<li>La password deve essere almeno di 8 caratteri e non pu&ograve; contenere caratteri speciali</li>";
+    }
+    if(passwordconfirm==null || passwordconfirm=="" || passwordconfirm != password ){
+    	erroriForm = erroriForm + "<li>La password di conferma non corrisponde!</li>";
+    }
+
+    if(erroriForm != "" ) {
+    	erroriForm = "<p>Sono stati riscontrati i seguenti errori:</p><ul>".concat(erroriForm);
+    	erroriForm.concat("</ul>");
+    	campoErrori.innerHTML = erroriForm;
+    	return false;
+    }
+};
